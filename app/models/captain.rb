a52eedb4d088b5a24603078dc314ae690e2537d1
jkhoang313 <jkhoang313@gmail.com>
2016-12-13 21:25:24 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+  def self.catamaran_operators
+    self.joins(:classifications).where("classifications.name = 'Catamaran'")
+  end
+
+  def self.sailors
+    self.joins(:classifications).where(classifications: {name: "Sailboat"}).distinct
+    # self.joins(:classifications).where("classifications.name = 'Sailboat'").uniq
+  end
+
+  def self.talented_seamen
+    self.joins(:classifications).where("classifications.name = 'Motorboat' OR classifications.name = 'Sailboat'").group("classifications.name").order("captains.name asc")
+  end
+
+  def self.non_sailors
+    sailors = self.joins(:classifications).where("classifications.name = 'Sailboat'").uniq
+    sailors_insert = sailors.pluck(:name).collect {|name| "'" + name + "'"}.join(" AND captains.name != ")
+    self.joins(:classifications).where("captains.name != #{sailors_insert}").uniq
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self
+  end
+
+  def self.longest
+    longest_length = self.joins(:boats).order("boats.length desc").pluck("boats.length").first
+    self.joins(:boats).where("length = #{longest_length}")
+  end
 end
